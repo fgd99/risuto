1680c4ac0907f22bbb8d1e0de80c7000db879637
fgd99 <fabriceguydominguez@gmail.com>
2015-11-02 13:47:00 +0100

    starting the project
    hello_world example to test the setup

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * HelloWorld example
+ * compilation: cc -std=c99 -Wall hello_world.c -o hello_world
+ **/
+
+int main(int argc, char** argv) {
+  puts("Hello, world!");
+  return 0;
+}
