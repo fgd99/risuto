42610b0af9d2650c5f3013473b64e33c3ce61ed1
Fabrice <fabriceguydominguez@gmail.com>
2015-11-03 13:38:28 +0100

    Utilisation de la libraire editline sous linux

--- prompt.c
+++ prompt.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-
 /**
- * Exemple de prompt
- * compilation: cc -std=c99 -Wall prompt.c -o prompt
+ * Basic prompt using editline to provide a command history
+ * compilation: cc -std=c99 -Wall prompt.c -ledit -o prompt
  **/
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
 
@@ -17,14 +18,18 @@ int main(int argc, char** argv) {
   /* In a never ending loop */
   while (1) {
 
-    /* Output our prompt */
-    fputs("lispy> ", stdout);
+    /* Output our prompt and get input */
+    char* input = readline("lispy> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* Add input to history */
+    add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
+
   }
 
   return 0;
